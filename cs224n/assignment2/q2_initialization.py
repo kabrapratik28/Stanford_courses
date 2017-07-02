800db9722132c19f7ddc49c57bd07a8582180aaf
Pratik Kabara <kabrapratik28@gmail.com>
2017-07-02 01:13:48 -0700

    xavier weight initializer
    xavier weight initializer

--- cs224n/assignment2/q2_initialization.py
+++ cs224n/assignment2/q2_initialization.py
@@ -25,6 +25,8 @@ def xavier_weight_init():
             out: tf.Tensor of specified shape sampled from the Xavier distribution.
         """
         ### YOUR CODE HERE
+        e = 6**(0.5) / (sum(shape))**(0.5)
+        out = tf.random_uniform(shape,minval=-e,maxval=e)
         ### END YOUR CODE
         return out
     # Returns defined initializer function.
