bd05101ef8ac30666c48382fea83208e1bdfd7b7
Pratik Kabara <kabrapratik28@gmail.com>
2017-07-04 14:16:48 -0700

    Warm up exercise
    Warm up exercise

--- cs20si/tf-stanford-tutorials/assignments/exercises/a1_q1_exercises.py
+++ cs20si/tf-stanford-tutorials/assignments/exercises/a1_q1_exercises.py
@@ -22,7 +22,11 @@ out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
 # Hint: Look up tf.case().
 ###############################################################################
 
-# YOUR CODE
+x = tf.random_uniform([],minval=-1,maxval=1)
+y = tf.random_uniform([],minval=-1,maxval=1)
+out = tf.case([(tf.less(x,y), lambda: x+y),
+               (tf.greater(x,y), lambda: x-y)],
+               default=lambda : tf.constant(0, dtype=tf.float32))
 
 ###############################################################################
 # 1c: Create the tensor x of the value [[0, -2, -1], [0, 1, 2]] 
@@ -31,7 +35,9 @@ out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
 # Hint: Look up tf.equal().
 ###############################################################################
 
-# YOUR CODE
+x = tf.Variable([[0, -2, -1], [0, 1, 2]])
+y = tf.zeros_like(x)
+out = tf.equal(x,y)
 
 ###############################################################################
 # 1d: Create the tensor x of value 
@@ -46,7 +52,13 @@ out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
 # Hint: Use tf.gather().
 ###############################################################################
 
-# YOUR CODE
+x = tf.Variable([29.05088806,  27.61298943,  31.19073486,  29.35532951,
+                 30.97266006,  26.67541885,  38.08450317,  20.74983215,
+                 34.94445419,  34.45999146,  29.06485367,  36.01657104,
+                 27.88236427,  20.56035233,  30.20379066,  29.51215172,
+                 33.71149445,  28.59134293,  36.05556488,  28.66994858])
+y = tf.where(x > 30)
+out = tf.gather(x,y)
 
 ###############################################################################
 # 1e: Create a diagnoal 2-d tensor of size 6 x 6 with the diagonal values of 1,
@@ -54,7 +66,7 @@ out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
 # Hint: Use tf.range() and tf.diag().
 ###############################################################################
 
-# YOUR CODE
+out = tf.diag(tf.range(1,7))
 
 ###############################################################################
 # 1f: Create a random 2-d tensor of size 10 x 10 from any distribution.
@@ -62,7 +74,8 @@ out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
 # Hint: Look at tf.matrix_determinant().
 ###############################################################################
 
-# YOUR CODE
+x = tf.random_uniform(shape=(10,10))
+out = tf.matrix_determinant(x)
 
 ###############################################################################
 # 1g: Create tensor x with value [5, 2, 3, 5, 10, 6, 2, 3, 4, 2, 1, 1, 0, 9].
@@ -70,7 +83,8 @@ out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
 # Hint: use tf.unique(). Keep in mind that tf.unique() returns a tuple.
 ###############################################################################
 
-# YOUR CODE
+x = tf.Variable([5, 2, 3, 5, 10, 6, 2, 3, 4, 2, 1, 1, 0, 9])
+y, idx = tf.unique(x)
 
 ###############################################################################
 # 1h: Create two tensors x and y of shape 300 from any normal distribution,
@@ -82,4 +96,10 @@ out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
 # Hint: see the Huber loss function in the lecture slides 3.
 ###############################################################################
 
-# YOUR CODE
+x = tf.random_uniform(shape=[300])
+y = tf.random_uniform(shape=[300])
+boolean = tf.less(tf.reduce_mean(x-y),tf.constant(0,dtype=tf.float32))
+diff = x-y
+mean_squared_error = tf.reduce_mean(tf.square(diff)) 
+sum_abs_val = tf.reduce_sum(tf.abs(diff))
+out = tf.cond(boolean,lambda : mean_squared_error,lambda : sum_abs_val)
